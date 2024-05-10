4f0067c8ce55f015b173c116f92df574ddaa2fb8
yashas8197 <yashasjr7@gmail.com>
2024-05-10 19:10:50 +0530

    feat: added menuClose action to appSlice

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { menuClose } from "../utils/appSlice";
 
 const WatchPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(menuClose());
+  }, []);
   return <div>WatchPage</div>;
 };
 
--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -9,8 +9,11 @@ const appSlice = createSlice({
     toggleMenu: (state) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
+    menuClose: (state) => {
+      state.isMenuOpen = false;
+    },
   },
 });
 
-export const { toggleMenu } = appSlice.actions;
+export const { toggleMenu, menuClose } = appSlice.actions;
 export default appSlice.reducer;
